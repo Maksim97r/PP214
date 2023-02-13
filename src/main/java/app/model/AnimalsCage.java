23aecb81b5183d27a7f9d9e34c594b90bc1f4769
maksim97r <game452@mail.ru>
2023-02-13 11:13:32 +0300

    PP214

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,12 +1,18 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    @Autowired
+    private Timer timer;
+
 
     @Autowired
+    @Qualifier("dogBean")
     private Animal animal;
 
     public void whatAnimalSay() {
@@ -15,5 +21,10 @@ public class AnimalsCage {
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+    }
+
+    @Bean
+    public Timer getTimer() {
+        return timer;
     }
 }
---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,9 @@
+package app.model;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component("dogBean")
+public class Dog extends Animal{
+
+}
